4519290c870f7eb44368f23e83dc3a5815d63df4
Mangereza <theafricansensation@icloud.com>
2022-05-25 09:36:50 +0300

    Assignment4.py

--- Assignment4.py
+++ Assignment4.py
@@ -7,13 +7,16 @@
 ######################
 
 acc_bal= input("enter your acc_bal")
-acc_bal=30000
+
 if  (int(acc_bal) > 100000 and int(acc_bal) < 200000):
      acc_bal = acc_bal - 25000
-     print(" we have deducted 25000")
+     print(" we have deducted ksh 25000/n")
+     print("your new balance is 25000")
 elif (int(acc_bal) > 500000 and int(acc_bal) < 1000000):
-       acc_bal = acc_bal - float(0.3)*acc_bal
+       acc_bal = float(acc_bal) - (0.3*float(acc_bal))
        print(" we have deducted 30 percent from your account")
 elif (int(acc_bal) > 1000000):
     acc_bal = acc_bal- 15000
     print(" We have deducted ksh15000 from your account")
+else :
+    print("No deduction done")
---
+++ Assignment5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#a list with different types of cars 
+
+vehicles=['bmw','audi','toyota','mercedes','jeep']
+
+#using a for loop to print  all vehicles
+for vehicle in vehicles:
+    if (vehicle=="jeep"):
+        print(vehicle.upper())
+
+
+
+
+
