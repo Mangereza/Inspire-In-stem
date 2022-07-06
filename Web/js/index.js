a942ba69ed41784af5c1da6bdf437519c02e9eca
Mangereza <theafricansensation@icloud.com>
2022-07-06 11:03:32 +0300

    index.js

---
+++ Web/js/index.js
@@ -0,0 +1,12 @@
+let message ="Hello world"
+console.log(message)
+//single line comment in Javascript
+/* 
+Multiline comment in Javascript
+*/
+let num1 = 134
+let num2 = 134
+
+function submitButton(){
+    alert("Button has been clicked")
+}
